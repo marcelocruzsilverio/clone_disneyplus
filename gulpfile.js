eecf7426d9f288873b93b94738c5f854bec7df3a
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2023-06-23 20:28:20 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,29 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemim = require('gulp-imagemin')
+const uglify = require('gulp-uglify')
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/css'));
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
 }
 
 function images() {
     return gulp.src('./src/images/**/*')
-    .pipe(imagemim())
-    .pipe(gulp.dest('./dist/images'));
+        .pipe(imagemim())
+        .pipe(gulp.dest('./dist/images'));
 }
 
 
-exports.default = gulp.parallel(styles, images);
-exports.watch = function() {
+exports.default = gulp.parallel(styles, images, scripts);
+exports.watch = function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
